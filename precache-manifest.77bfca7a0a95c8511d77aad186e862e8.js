d03cb25eea3be890861809319c1012ee34cbd00e
acebogo <koprodev@gmail.com>
2020-10-22 16:49:34 +0900

    Updates

---
+++ precache-manifest.77bfca7a0a95c8511d77aad186e862e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097c663d288a1aaf12c092812e3ee28f",
+    "url": "/react-first-app/index.html"
+  },
+  {
+    "revision": "15a16a038ff500464909",
+    "url": "/react-first-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7def507664f10a49ca62",
+    "url": "/react-first-app/static/js/2.bc571bee.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-first-app/static/js/2.bc571bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a16a038ff500464909",
+    "url": "/react-first-app/static/js/main.4b88fee9.chunk.js"
+  },
+  {
+    "revision": "7849673ddf9da96e0973",
+    "url": "/react-first-app/static/js/runtime-main.410da258.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-first-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
